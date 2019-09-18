47eeb9e7ff424d31638f126faf44bc205c519e81
Lampppp <ldkoog00@harrisonburg.k12.va.us>
2019-09-18 09:27:08 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,25 @@
-var score = 1; // Create a variable to store the score in
+var score = 5;
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var scr = new Text(score);                    // Create a text element
+postToPage(score);
 
-setInterval(btnPress, 1000);   // Set up a loop
+createButton('1', btnPress);
+createButton('-1', btnPress2);
+changeTitle('Clicker Game');
+
+setInterval(btnPress, 1000);
 
 function btnPress() {
-  score++;         // Increase the score
-  scr.edit(score); // Update the page with the new score
+  score = score + 1;
+  postToPage(score);
+}
+function btnPress2() {
+  score = score + -0.25;
+  postToPage(score);
+}
+
+if (score <= 0){
+  textWeight(10);
+  
+  text(You win);
+
 }
