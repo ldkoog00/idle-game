fd0aa79b1a2f8d5acd782c72bea12e97b8739ccf
Lampppp <ldkoog00@harrisonburg.k12.va.us>
2019-09-19 09:43:34 -0400

    bade clicker game

--- game.js
+++ game.js
@@ -1,25 +1,43 @@
 var score = 5;
 
-postToPage(score);
+var title = new Title('Clicker Game');
 
-createButton('1', btnPress);
-createButton('-1', btnPress2);
-changeTitle('Clicker Game');
+var add = new Button('1', btnPress);
+var sub = new Button('-1', btnPress2);
+var scoreBoard = new Text(score);
 
 setInterval(btnPress, 1000);
 
 function btnPress() {
   score = score + 1;
-  postToPage(score);
+  scoreBoard.edit(score);
+  if (score <= 0){
+    textWeight(10);
+    textStroke(0);
+    let message = new Text("You Win");
+  
+  }
+  if (score >= 10){
+    textWeight(10);
+    textStroke(0);
+    text(200, 200, "You Loose");
+  
+  }
 }
 function btnPress2() {
   score = score + -0.25;
-  postToPage(score);
-}
-
-if (score <= 0){
-  textWeight(10);
+  scoreBoard.edit(score);
+  if (score <= 0){
+    textWeight(10);
+    textStroke(0);
+    text(200, 200, "You Win");
   
-  text(You win);
-
+  }
+  if (score >= 10){
+    textWeight(10);
+    textStroke(0);
+    text(200, 200, "You Loose");
+  
+  }
 }
+
