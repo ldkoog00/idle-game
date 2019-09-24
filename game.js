e55b39b406da1354f8d45d62a23fe89c290a4e6a
Lampppp <ldkoog00@harrisonburg.k12.va.us>
2019-09-24 09:53:53 -0400

    fbffd

--- game.js
+++ game.js
@@ -1,55 +1,68 @@
-var score = 5;
+var cats = 1;
+var deadCats = 0;
+var graveyardArea = 100;
+var expandableArea = 0;
 
 var title = new Title('clicker game');
 
-var rowSection = new Section('row');
-var column1 = new Section('col');    
-var column2 = new Section('col'); 
+var rowSectionA = new Section('row');
+var rowSectionB = new Section('row');
+var rowSectionC = new Section('row');
+var rowSectionD = new Section('row');
+var column1A = new Section('col');    
+var column2A = new Section('col'); 
+var column1B = new Section('col');    
+var column2B = new Section('col'); 
+var column1C = new Section('col');    
+var column2C = new Section('col'); 
+var column1D = new Section('col');    
+var column2D = new Section('col'); 
 
 var addCat = new Button('Adopt Cat', btnPress);
-var sub = new Button('-1', btnPress2);
-var scoreBoard = new Text(score);
+var burryCat = new Button('Burry Cat', btnPress2);
+var killCat = new Button('Kill Cat', btnPress3);
+var expandGraveyard = new Button('expand graveyard', btnPress4);
+var numberOfCats = new Text("Cats: " + cats);
+var numberOfDeadCats = new Text("Dead Cats: " + deadCats);
 
-column1.add(addCat);
-column1.add(sub);
-column2.add(scoreboard);
+column1A.add(addCat);
+column1B.add(burryCat);
+column2A.add(numberOfCats);
+column2B.add(numberOfDeadCats);
 
-rowSection.add(column1);  
-rowSection.add(column2);
+rowSectionA.add(column1A);  
+rowSectionA.add(column2A);
+rowSectionB.add(column1B);  
+rowSectionB.add(column2B);
+rowSectionC.add(column1C);  
+rowSectionC.add(column2C);
+rowSectionD.add(column1D);  
+rowSectionD.add(column2D);
 
-
-setInterval(btnPress, 1000);
+setInterval (btnPress3, 10000)
 
 function btnPress() {
-  score = score + 1;
-  scoreBoard.edit(score);
-  if (score <= 0){
-    textWeight(10);
-    textStroke(0);
-    let message = new Text("You Win");
+  cats = cats + 1;
+  numberOfCats.edit("Cats: " + cats);
   
-  }
-  if (score >= 10){
-    textWeight(10);
-    textStroke(0);
-    text("You Loose");
-  
-  }
 }
 function btnPress2() {
-  score = score + -0.25;
-  scoreBoard.edit(score);
-  if (score <= 0){
-    textWeight(10);
-    textStroke(0);
-    text("You Win");
+  deadCats = deadCats + -1;
+  numberOfDeadCats.edit("Dead Cats: " + deadCats);
+ 
+}
+function btnPress3() {
+  cats = cats + -1;
+  deadCats = deadCats + 1;
+  numberOfCats.edit("Cats: " + cats);
+  numberOfDeadCats.edit("Dead Cats: " + deadCats);
+ 
+}
+function btnPress4() {
+  cats = cats + -1;
+  deadCats = deadCats + 1;
+  numberOfCats.edit("Graveyard Area: " + graveyardArea + "ft");
   
-  }
-  if (score >= 10){
-    textWeight(10);
-    textStroke(0);
-    text("You Loose");
-  
-  }
+ 
 }
 
